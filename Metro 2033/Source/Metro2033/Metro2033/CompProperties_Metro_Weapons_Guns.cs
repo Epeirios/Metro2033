5e428035509b42ca5f8a83f9bea91bd3dd8ab05d
Kevin Heugen <kevinheugen1996@gmail.com>
2015-12-31 14:36:21 +0100

    Adding Mod Files - v0001

---
+++ Metro 2033/Source/Metro2033/Metro2033/CompProperties_Metro_Weapons_Guns.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using UnityEngine;
+using Verse;
+using RimWorld;
+
+namespace Metro2033
+{
+    public class CompProperties_Metro_Weapons_Guns : CompProperties
+    {
+        public int magRounds = 1;
+    }
+}
