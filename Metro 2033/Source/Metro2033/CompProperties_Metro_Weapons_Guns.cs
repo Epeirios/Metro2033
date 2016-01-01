1c9d39db29471de16b2caab641d7d3c339c8d477
Kevin Heugen <kevinheugen1996@gmail.com>
2016-01-01 01:17:21 +0100

    v0003
    - Starting on adding a Ammo Property for FireWeapons
    - Fix some bugs in Metro_Weapons_Guns.xml
    - Added CommunityCoreLibrary.dll

--- Metro 2033/Source/Metro2033/CompProperties_Metro_Weapons_Guns.cs
+++ Metro 2033/Source/Metro2033/CompProperties_Metro_Weapons_Guns.cs
@@ -3,13 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-using UnityEngine;
 using Verse;
-using RimWorld;
 
 namespace Metro2033
 {
-    public class CompProperties_Metro_Weapons_Guns : CompProperties
+    class CompProperties_Metro_Weapons_Guns : CompProperties
     {
         public int magRounds = 1;
     }
