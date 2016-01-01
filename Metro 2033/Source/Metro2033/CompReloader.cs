9dbeb25a7c8bcb3b13382a7d20cd96db14b73d95
Kevin Heugen <kevinheugen1996@gmail.com>
2016-01-01 22:24:25 +0100

    v0005
    - trying to understand several dll's
    - changed some code

---
+++ Metro 2033/Source/Metro2033/CompReloader.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Verse;
+
+namespace Metro2033
+{
+    class CompReloader : ThingComp
+    {
+        public CompProperties_Metro_Weapons_Guns gunProperties;
+
+        public void Reload()
+        {
+            
+        }
+    }
+}
---
+++ Metro 2033/Source/Metro2033/Verb_Shoot_Metro_Weapon.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using RimWorld;
+using UnityEngine;
+using Verse;
+using Verse.AI;
+
+namespace Metro2033
+{
+    class Verb_Shoot_Metro_Weapon : Verb_Shoot
+    {
+        private CompReloader compAmmo;     
+        
+
+    }
+}
